3100775f4b49697d89774d437f32c47927fe0b93
Vaibhav Dhoke <vaibhavdhoke1@gmail.com>
2021-03-12 11:04:12 -0500

    Comments added in app.js

--- app.js
+++ app.js
@@ -12,16 +12,24 @@ app.get("/", (req, res) => {
   res.send("Welcome to Human Care Systems");
 });
 
+
+//Importing the data from CSV to the collection
 ImpData.ImportData();
 
+
+//Calling the method to schedule the emails and storing in "emails" collection
 ScheduleEmail.ScheduleEmail().then(function(result) {
-  console.log(result) // "Some User token"
+  console.log(result) 
 })
 
+
+//Calling the method to find the missing Emails from the "patient" collection where CONSENT is Y
 missingEmail.missingEmail().then(function (result) {
-  console.log(result); // "Some User token"
+  console.log(result); 
 });
 
+
+//Calling the method to find out the patientIDs who has missing first names;
 findPatient.missingFName();
 
 
