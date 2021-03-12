12e04f7901f99b99455c1bb069c346544a82380e
Vaibhav Dhoke <vaibhavdhoke1@gmail.com>
2021-03-11 21:22:45 -0500

    Change in app.js

--- app.js
+++ app.js
@@ -8,33 +8,27 @@ const DropData = require("./Modules/DropCSVCollection");
 const ScheduleEmail = require("./Modules/SchedulEmail");
 const missingEmail = require("./Modules/MissingEmails");
 
-const mongodb = require("mongodb").MongoClient;
-var url = "mongodb://localhost:27017/";
-
-const csvtojson = require("csvtojson");
-
-// app.set("view engine", "hbs");
-
 app.get("/", (req, res) => {
   res.send("Welcome to Human Care Systems");
 });
+
+ImpData.ImportData();
+
+ScheduleEmail.ScheduleEmail().then(function(result) {
+  console.log(result) // "Some User token"
+})
 
 missingEmail.missingEmail().then(function (result) {
   console.log(result); // "Some User token"
 });
 
-// ImpData.ImportData();
-// patientData.ViewPatientCollection();
+findPatient.missingFName();
+
+
 // DropData.DropCollection().then(function(result) {
 //     console.log(result) // "Some User token"
 //  })
 
-// ScheduleEmail.ScheduleEmail().then(function(result) {
-//     console.log(result) // "Some User token"
-//  })
-
-// DropData.DropCollection();
-// findPatient.missingFName();
 
 app.listen(3001, () => {
   console.log("Server Started on Port 3001");
